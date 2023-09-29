d96439313dfa9eef7b738078269a5923c2d24403
Imad Eddine Kettaf <imedkettaf@gmail.com>
2023-09-29 14:57:50 +0100

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// code your solution here
+function superbowlWin(objArr) {
+    const result = objArr.find(obj => {
+        return obj.result === "W"
+    })
+
+    if (result)
+        return result.year;
+    return result;
+}
